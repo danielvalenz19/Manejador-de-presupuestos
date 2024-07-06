ac7ea67c562345c0cd32a46e9f1aee316c9a88ef
Daniel Valenzuela <hetzondaniel1@gmial.com>
2024-07-05 19:40:01 -0600

    primer commit se agrego la vista para crear tipos de cuentas

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+	public class TipoCuenta
+	{
+        public int Id { get; set; }
+		[Required(ErrorMessage ="El campo {0} es requerido")]
+		[StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage ="La longitud del campo {0} debe de estar {2}")]
+		public string Nombre { get; set; }
+		public int Usuario { get; set; }
+		public int Orden { get; set; }
+
+	}
+}
